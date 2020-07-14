ca28fc3efc64cec0d4a0bbc0755ce22060534ff6
Peter Steenbergen <psteenbergen@gmail.com>
2020-07-14 16:32:36 +0200

    Getters and Setters for addInvoice

--- src/Models/EboekhoudenInvoice.php
+++ src/Models/EboekhoudenInvoice.php
@@ -22,6 +22,18 @@ class EboekhoudenInvoice implements Arrayable
     protected string $email_from_address = '';
     protected string $email_from_name = '';
     protected bool $automatic_incasso = false;
+    protected string $incasso_iban = '';
+    protected string $incasso_mandate_kind = '';
+    protected string $incasso_mandate_id = '';
+    protected ?DateTime $incasso_mandate_signature_date = null;
+    protected bool $incasso_mandate_first = false;
+    protected string $incasso_account_number = '';
+    protected string $incasso_account_holder = '';
+    protected string $incasso_city = '';
+    protected string $incasso_description_row_1 = '';
+    protected string $incasso_description_row_2 = '';
+    protected string $incasso_description_row_3 = '';
+    protected bool $process_in_ledger = false;
     protected string $mutation_description = '';
     protected array $lines = [];
 
@@ -34,7 +46,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $invoice_number
+     * @param  string  $invoice_number
      * @return EboekhoudenInvoice
      */
     public function setInvoiceNumber(string $invoice_number): EboekhoudenInvoice
@@ -53,7 +65,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $relation_code
+     * @param  string  $relation_code
      * @return EboekhoudenInvoice
      */
     public function setRelationCode(string $relation_code): EboekhoudenInvoice
@@ -64,15 +76,19 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @return DateTime|null
+     * @return string
      */
-    public function getDate(): ?DateTime
+    public function getDate(): string
     {
-        return $this->date;
+        if (is_null($this->date)) {
+            return (new DateTime())->format('Y-m-d');
+        }
+
+        return $this->date->format('Y-m-d');
     }
 
     /**
-     * @param DateTime|null $date
+     * @param  DateTime|null  $date
      * @return EboekhoudenInvoice
      */
     public function setDate(?DateTime $date): EboekhoudenInvoice
@@ -91,7 +107,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param int $payment_term
+     * @param  int  $payment_term
      * @return EboekhoudenInvoice
      * @throws EboekhoudenException
      */
@@ -115,7 +131,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $invoice_template
+     * @param  string  $invoice_template
      * @return EboekhoudenInvoice
      */
     public function setInvoiceTemplate(string $invoice_template): EboekhoudenInvoice
@@ -134,7 +150,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param bool $send_per_email
+     * @param  bool  $send_per_email
      * @return EboekhoudenInvoice
      */
     public function setSendPerEmail(bool $send_per_email): EboekhoudenInvoice
@@ -153,7 +169,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $email_subject
+     * @param  string  $email_subject
      * @return EboekhoudenInvoice
      */
     public function setEmailSubject(string $email_subject): EboekhoudenInvoice
@@ -172,7 +188,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $email_message
+     * @param  string  $email_message
      * @return EboekhoudenInvoice
      */
     public function setEmailMessage(string $email_message): EboekhoudenInvoice
@@ -191,7 +207,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $email_from_address
+     * @param  string  $email_from_address
      * @return EboekhoudenInvoice
      */
     public function setEmailFromAddress(string $email_from_address): EboekhoudenInvoice
@@ -210,7 +226,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $email_from_name
+     * @param  string  $email_from_name
      * @return EboekhoudenInvoice
      */
     public function setEmailFromName(string $email_from_name): EboekhoudenInvoice
@@ -229,12 +245,244 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param bool $automatic_incasso
+     * @param  bool  $automatic_incasso
      * @return EboekhoudenInvoice
      */
     public function setAutomaticIncasso(bool $automatic_incasso): EboekhoudenInvoice
     {
         $this->automatic_incasso = $automatic_incasso;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoIban(): string
+    {
+        return $this->incasso_iban;
+    }
+
+    /**
+     * @param  string  $incasso_iban
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoIban(string $incasso_iban): EboekhoudenInvoice
+    {
+        $this->incasso_iban = $incasso_iban;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoMandateKind(): string
+    {
+        return $this->incasso_mandate_kind;
+    }
+
+    /**
+     * @param  string  $incasso_mandate_kind
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoMandateKind(string $incasso_mandate_kind): EboekhoudenInvoice
+    {
+        $this->incasso_mandate_kind = $incasso_mandate_kind;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoMandateId(): string
+    {
+        return $this->incasso_mandate_id;
+    }
+
+    /**
+     * @param  string  $incasso_mandate_id
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoMandateId(string $incasso_mandate_id): EboekhoudenInvoice
+    {
+        $this->incasso_mandate_id = $incasso_mandate_id;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoMandateSignatureDate(): string
+    {
+        if (! is_null($this->incasso_mandate_signature_date)) {
+            return $this->incasso_mandate_signature_date->format('y-m-d');
+        }
+
+        return (new DateTime('1970-01-01 00:00:00'))->format('Y-m-d');
+    }
+
+    /**
+     * @param  DateTime|null  $incasso_mandate_signature_date
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoMandateSignatureDate(?DateTime $incasso_mandate_signature_date): EboekhoudenInvoice
+    {
+        $this->incasso_mandate_signature_date = $incasso_mandate_signature_date;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getIncassoMandateFirst(): bool
+    {
+        return $this->incasso_mandate_first;
+    }
+
+    /**
+     * @param  bool  $incasso_mandate_first
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoMandateFirst(bool $incasso_mandate_first): EboekhoudenInvoice
+    {
+        $this->incasso_mandate_first = $incasso_mandate_first;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoAccountNumber(): string
+    {
+        return $this->incasso_account_number;
+    }
+
+    /**
+     * @param  string  $incasso_account_number
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoAccountNumber(string $incasso_account_number): EboekhoudenInvoice
+    {
+        $this->incasso_account_number = $incasso_account_number;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoAccountHolder(): string
+    {
+        return $this->incasso_account_holder;
+    }
+
+    /**
+     * @param  string  $incasso_account_holder
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoAccountHolder(string $incasso_account_holder): EboekhoudenInvoice
+    {
+        $this->incasso_account_holder = $incasso_account_holder;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoCity(): string
+    {
+        return $this->incasso_city;
+    }
+
+    /**
+     * @param  string  $incasso_city
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoCity(string $incasso_city): EboekhoudenInvoice
+    {
+        $this->incasso_city = $incasso_city;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoDescriptionRow1(): string
+    {
+        return $this->incasso_description_row_1;
+    }
+
+    /**
+     * @param  string  $incasso_description_row_1
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoDescriptionRow1(string $incasso_description_row_1): EboekhoudenInvoice
+    {
+        $this->incasso_description_row_1 = $incasso_description_row_1;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoDescriptionRow2(): string
+    {
+        return $this->incasso_description_row_2;
+    }
+
+    /**
+     * @param  string  $incasso_description_row_2
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoDescriptionRow2(string $incasso_description_row_2): EboekhoudenInvoice
+    {
+        $this->incasso_description_row_2 = $incasso_description_row_2;
+
+        return $this;
+    }
+
+    /**
+     * @return string
+     */
+    public function getIncassoDescriptionRow3(): string
+    {
+        return $this->incasso_description_row_3;
+    }
+
+    /**
+     * @param  string  $incasso_description_row_3
+     * @return EboekhoudenInvoice
+     */
+    public function setIncassoDescriptionRow3(string $incasso_description_row_3): EboekhoudenInvoice
+    {
+        $this->incasso_description_row_3 = $incasso_description_row_3;
+
+        return $this;
+    }
+
+    /**
+     * @return bool
+     */
+    public function getProcessInLedger(): bool
+    {
+        return $this->process_in_ledger;
+    }
+
+    /**
+     * @param  bool  $process_in_ledger
+     * @return EboekhoudenInvoice
+     */
+    public function setProcessInLedger(bool $process_in_ledger): EboekhoudenInvoice
+    {
+        $this->process_in_ledger = $process_in_ledger;
 
         return $this;
     }
@@ -248,7 +496,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param string $mutation_description
+     * @param  string  $mutation_description
      * @return EboekhoudenInvoice
      */
     public function setMutationDescription(string $mutation_description): EboekhoudenInvoice
@@ -271,7 +519,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param EboekhoudenInvoiceLine $line
+     * @param  EboekhoudenInvoiceLine  $line
      * @return EboekhoudenInvoice
      */
     public function addLine(EboekhoudenInvoiceLine $line): EboekhoudenInvoice
@@ -286,7 +534,7 @@ class EboekhoudenInvoice implements Arrayable
     }
 
     /**
-     * @param array $lines
+     * @param  array  $lines
      * @return EboekhoudenInvoice
      * @throws EboekhoudenException
      */
@@ -294,7 +542,7 @@ class EboekhoudenInvoice implements Arrayable
     {
         foreach ($lines as $line) {
             if (! ($line instanceof EboekhoudenInvoiceLine)) {
-                throw new EboekhoudenException('All invoice lines must be instance of ' . EboekhoudenInvoiceLine::class);
+                throw new EboekhoudenException('All invoice lines must be instance of '.EboekhoudenInvoiceLine::class);
             }
         }
         $this->lines = $lines;
